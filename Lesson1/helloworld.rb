c97c6f1698f0083d8d5e01776ac4cc920ad829b7
Stephen McCullan <smccullan@Stephens-MacBook-Pro.local>
2013-03-25 19:41:25 +1100

    First commit

---
+++ Lesson1/helloworld.rb
@@ -0,0 +1,15 @@
+first_name = 1
+last_name = 1
+age = 1
+
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts "What is your age?"
+age = gets.chomp
+
+
+puts "Hello #{first_name} #{last_name} , you are #{age}"
